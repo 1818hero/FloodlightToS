9983856d8c5046c382640957e7d39bb8f4c86321
weking <wekingzhang@gmail.com>
2015-06-09 20:12:34 +0800

    delete the energy saving and linkcost source code

--- src/main/java/net/floodlightcontroller/linkCostService/ILinkCostService.java
+++
@@ -1,9 +0,0 @@
-package net.floodlightcontroller.linkCostService;
-
-import net.floodlightcontroller.core.module.IFloodlightService;
-import net.floodlightcontroller.routing.Link;
-import java.util.Map;
-
-public interface ILinkCostService extends IFloodlightService {
-	public Map<Link,Integer> getLinkCost();
-}
